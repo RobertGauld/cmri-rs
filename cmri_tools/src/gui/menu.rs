5c209f66b7dcf1c9e25bc0fb280cc0187b1df492
Robert Gauld <robert@robertgauld.uk>
2025-03-07 20:57:34 +0000

    Fix save dialog

--- cmri_tools/src/gui/menu.rs
+++ cmri_tools/src/gui/menu.rs
@@ -40,7 +40,7 @@ pub mod file {
     /// Makes use of modal for displaying any errors which occured.
     pub fn save_nodes(ui: &mut egui::Ui, modal: &egui_modal::Modal, file_path: &mut Option<std::path::PathBuf>, get: impl Fn() -> Vec<crate::file::Node>) {
         if ui.button("Save Nodes").clicked() {
-            if let Some(file) = file_prompt("Save Nodes", file_path.as_ref()).pick_file() {
+            if let Some(file) = file_prompt("Save Nodes", file_path.as_ref()).save_file() {
                 match crate::file::save_nodes(&file, get()).context("Failed to save network.") {
                     Err(error) => modal_error(modal, &error),
                     Ok(()) => { file_path.replace(file); }
